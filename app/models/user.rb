1033259310c9d714535aa0ceb0e251d3f2791f61
Juan Antonio Galisteo <jgalisteo@nosolosoftware.biz>
2015-03-03 16:33:57 +0100

    No se confirman los users, de momento.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   include Mongoid::Timestamps
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :omniauthable and :rememberable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable, #:confirmable,
          :recoverable, :trackable, :validatable
 
   ## Database authenticatable
@@ -25,10 +25,10 @@ class User
   field :last_sign_in_ip,    type: String
 
   ## Confirmable
-  field :confirmation_token,   type: String
-  field :confirmed_at,         type: Time
-  field :confirmation_sent_at, type: Time
-  field :unconfirmed_email,    type: String # Only if using reconfirmable
+  #field :confirmation_token,   type: String
+  #field :confirmed_at,         type: Time
+  #field :confirmation_sent_at, type: Time
+  #field :unconfirmed_email,    type: String # Only if using reconfirmable
 
   ## Lockable
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
