08b881e8a3e828f8e04362e1f88989f4ebfaed28
Juan Antonio Galisteo <jgalisteo@nosolosoftware.biz>
2015-03-04 10:37:37 +0100

    Comentarios en el controlador de usuarios.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,16 @@ class UsersController < Devise::RegistrationsController
 
   # POST /resource
   def create
+    # resource es un user
     build_resource(sign_up_params)
 
-    resource_saved = resource.save
+    resource_saved = resource.save # true si el usuario se crea y false si no
     yield resource if block_given?
     if resource_saved
       expire_data_after_sign_in!
       resource.ensure_authentication_token!
 
+      # 201 y envío un json con el usuario
       respond_with resource, location: after_inactive_sign_up_path_for(resource)
     else
       clean_up_passwords resource
@@ -18,6 +20,7 @@ class UsersController < Devise::RegistrationsController
       if @validatable
         @minimum_password_length = resource_class.password_length.min
       end
+      # 422 porque el usuario tiene errores
       respond_with resource
     end
   end
